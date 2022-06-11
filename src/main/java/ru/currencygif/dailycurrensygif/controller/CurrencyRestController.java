d26055e7b5ccb206432a6023b0929eb158dcc435
Yatsukin <79377539+Yatsukin@users.noreply.github.com>
2022-06-11 19:44:12 +0300

    Реализовал RESTовые контроллеры, убрал ненужные тестовые

--- src/main/java/ru/currencygif/dailycurrensygif/controller/CurrencyRestController.java
+++ src/main/java/ru/currencygif/dailycurrensygif/controller/CurrencyRestController.java
@@ -1,6 +1,7 @@
 package ru.currencygif.dailycurrensygif.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,29 +18,33 @@ public class CurrencyRestController {
 
     private final CurrencyInfoService currencyService;
     private final GiphyInfoService giphyInfoService;
+    @Value("${giphy.rich}")
+    private String rich;
+    @Value("${giphy.broke}")
+    private String broke;
+    @Value("${giphy.error}")
+    private String error;
+    @Value("${giphy.zero}")
+    private String nothing;
 
 
-    @GetMapping("/test")
-    public ResponseEntity<List> getCurrencyAll() {
+    @GetMapping("/getAllTicker")
+    public ResponseEntity<List> getTickers() {
         List<String> currencyInfo = currencyService.getCurrencyTicker();
         return ResponseEntity.ok(currencyInfo);
     }
 
-    @GetMapping("/compare/{ticker}")
-    public ResponseEntity<Integer> getComparisonInfo(@PathVariable String ticker) {
+    @GetMapping("/tst/{ticker}")
+    public ResponseEntity<GiphyDto> getGifByTag(@PathVariable String ticker) {
         int result = currencyService.getCurrencyComparisonResult(ticker);
-        return ResponseEntity.ok(result);
+        GiphyDto giphyDto;
+        switch (result) {
+            case 1 -> giphyDto = giphyInfoService.getRandomGif(rich);
+            case 0 -> giphyDto = giphyInfoService.getRandomGif(nothing);
+            case -1 -> giphyDto = giphyInfoService.getRandomGif(broke);
+            default -> giphyDto = giphyInfoService.getRandomGif(error);
+        }
+        return ResponseEntity.ok(giphyDto);
     }
-
-    @GetMapping("/tst/{tag}")
-    public ResponseEntity<GiphyDto> getAllGif(@PathVariable String tag) {
-        GiphyDto randomGif = giphyInfoService.getRandomGif(tag);
-        return ResponseEntity.ok(randomGif);
-    }
-
-    /*@GetMapping("/currency")
-    public Map<String, Integer> getComparedCurrency() {
-        return service.getComparedKeysInfo();
-    }*/
 
 }
