46824a5eee81527484a6e650edae4ea2105b327d
Yatsukin <79377539+Yatsukin@users.noreply.github.com>
2022-06-12 03:01:45 +0300

    Проверка поднятие всего контекста приложения и провекра принятия случайной гифки

---
+++ src/test/java/ru/currencygif/dailycurrensygif/DailyCurrencyGifApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.currencygif.dailycurrensygif;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@DisplayName("Приложение должно уметь ")
+class DailyCurrencyGifApplicationTests {
+
+	@Test
+	@DisplayName("Поднимать контекст")
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/ru/currencygif/dailycurrensygif/service/GiphyInfoServiceImpl.java
@@ -0,0 +1,39 @@
+package ru.currencygif.dailycurrensygif.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import ru.currencygif.dailycurrensygif.client.ClientGiphy;
+import ru.currencygif.dailycurrensygif.dto.DataGifDto;
+import ru.currencygif.dailycurrensygif.dto.GiphyDto;
+import ru.currencygif.dailycurrensygif.dto.ImagesGifDto;
+import ru.currencygif.dailycurrensygif.dto.OriginalGifDto;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+@SpringBootTest
+@DisplayName("Сервис гифок должен ")
+public class GiphyInfoServiceImpl {
+
+    @Autowired
+    private GiphyInfoService giphyInfoService;
+    @MockBean
+    private ClientGiphy clientGiphy;
+
+    @Test
+    @DisplayName("выдавать случайную гифку")
+    public void testGetRandomGif() throws MalformedURLException {
+        OriginalGifDto originalGifDto = new OriginalGifDto("tstFrame", "hash", "100", new URL("https://www.google.com/"), "395");
+        ImagesGifDto imagesGifDto = new ImagesGifDto(originalGifDto);
+        DataGifDto dataGifDto = new DataGifDto("gif", new URL("https://www.google.com/"), "title", imagesGifDto);
+        GiphyDto giphyDto = new GiphyDto(dataGifDto);
+        Mockito.when(clientGiphy.getRandomGif("jCWylEFUh9HGNpdjaNlhLDqQAmejXW7j", "test")).thenReturn(giphyDto);
+        Assertions.assertEquals(giphyInfoService.getRandomGif("test"), giphyDto);
+    }
+
+}
